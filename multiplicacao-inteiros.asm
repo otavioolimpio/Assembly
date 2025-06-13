6abf80cffb3ab840c7c231b5c17e439d6a5a0c5e
Otávio Olímpio <otavioolimpio@outlook.com>
2025-06-13 12:41:00 -0300

    Update multiplicacao-inteiros.asm

--- multiplicacao-inteiros.asm
+++ multiplicacao-inteiros.asm
@@ -1,10 +1,15 @@
 .text
-	#realizando a multiplicação
-	li $t0, 100
+	# Declarando os valores dos registradores para a multiplicação
+	li $t0, 2
 	li $t1, 10
+	
+	# MultipLicando 2^10 (o valor nove significa a quanridade de passos a esquerda)
+	sll $s1, $t0, 9
+	
+	# Multiplicação simples 
 	mul $s0, $t0, $t1
 	
-	#Imprimindo na tela
+	#Imprimindo na tela a multiplicação simples
 	li $v0, 1
 	add $a0, $zero, $s0
 	syscall
